e50c0bfd43cecfbd6bfb29bd1107dd789ad0f54e
Ali Almasi <a710almasi@gmail.com>
2025-01-18 16:52:04 +0330

    Changed some settings
    Signed-off-by: Ali Almasi <A710almasi@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -11,23 +11,61 @@ registerAllModules() && console.error("couldn't register modules");
 import "handsontable/styles/handsontable.min.css";
 import "handsontable/styles/ht-theme-main.min.css";
 
-createRoot(document.getElementById("root")).render(
-	<StrictMode>
+const App = () => {
+	return (
 		<div className="ht-theme-main-dark-auto">
+			<h1>جدول فروش</h1>
 			<HotTable
-				data={[
-					["", "Tesla", "Volvo", "Toyota", "Ford"],
-					["2019", 10, 11, 12, 13],
-					["2020", 20, 11, 14, 13],
-					["2021", 30, 15, 12, 13]
-				]}
-				rowHeaders={true}
-				colHeaders={true}
-				height="auto"
+				data={[[10, 20, 30], [11, 11, 15], [12, 14, 12], [13, 13, 13], [null]]}
+				rowHeaders={["تسلا", "ولوو", "تویوتا", "فورد", "مجموع"]}
+				colHeaders={["2019", "2020", "2021"]}
+				height={"auto"}
+				colWidths={300}
+				width={1000}
 				autoWrapRow={true}
 				autoWrapCol={true}
-				licenseKey="non-commercial-and-evaluation"
+				manualColumnResize={true}
+				manualRowResize={true}
+				layoutDirection={"rtl"}
+				manualColumnMove={true}
+				filters={true}
+				dropdownMenu={true}
+				contextMenu={true}
+				manualColumnFreeze={true}
+				dragToScroll={true}
+				columnSummary={[
+					{
+						sourceColumn: 0,
+						type: "sum",
+						reversedRowCoords: true,
+						destinationRow: 0,
+						destinationColumn: 0
+					},
+					{
+						sourceColumn: 1,
+						type: "sum",
+						reversedRowCoords: true,
+						destinationRow: 0,
+						destinationColumn: 1
+					},
+					{
+						sourceColumn: 2,
+						type: "sum",
+						reversedRowCoords: true,
+						destinationRow: 0,
+						destinationColumn: 2
+					}
+				]}
+				rowHeaderWidth={100}
+				enterBeginsEditing={true}
+				columns={[{type: "numeric", readOnly: true}, {type: "numeric"}, {type: "numeric"}]}
 			/>
 		</div>
+	);
+};
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
 	</StrictMode>
 );
