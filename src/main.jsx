cdb411395d55953247eacbf9962d046215e38e28
Ali Almasi <A710almasi@gmail.com>
2025-01-18 12:25:45 +0330

    https://stackoverflow.com/questions/79366657/hottable-component-not-rendering-while-passing-non-commercial-and-evaluation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Handsontable from "handsontable/base";
+import { registerAllModules } from "handsontable/registry";
+import { HotTable } from "@handsontable/react-wrapper";
+
+registerAllModules() && console.error("couldn't register modules");
+
+import "handsontable/styles/handsontable.min.css";
+import "handsontable/styles/ht-theme-main.min.css";
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<div className="ht-theme-main-dark-auto">
+			<HotTable
+				data={[
+					["", "Tesla", "Volvo", "Toyota", "Ford"],
+					["2019", 10, 11, 12, 13],
+					["2020", 20, 11, 14, 13],
+					["2021", 30, 15, 12, 13]
+				]}
+				rowHeaders={true}
+				colHeaders={true}
+				height="auto"
+				autoWrapRow={true}
+				autoWrapCol={true}
+				licenseKey="non-commercial-and-evaluation"
+			/>
+		</div>
+	</StrictMode>
+);
