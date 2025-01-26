cbe60f7e2fc634cddef3c0dca1a589c9d67335e5
Ali Almasi <A710almasi@gmail.com>
2025-01-26 17:09:10 +0330

    added data to table

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from "react";
+import { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import Handsontable from "handsontable/base";
+// import Handsontable from "handsontable/base";
 import { registerAllModules } from "handsontable/registry";
 import { HotTable } from "@handsontable/react-wrapper";
 
@@ -12,16 +12,36 @@ import "handsontable/styles/handsontable.min.css";
 import "handsontable/styles/ht-theme-main.min.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("handsontable");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+          const url = "http://localhost:3002/product_list";
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+        setName(url.split("/").reverse()[0]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
 	return (
-		<div className="ht-theme-main-dark-auto">
-			<h1>جدول فروش</h1>
+		<div className="ht-theme-main-dark-auto ">
+			<h1>{name}</h1>
 			<HotTable
-				data={[[10, 20, 30], [11, 11, 15], [12, 14, 12], [13, 13, 13], [null]]}
-				rowHeaders={["تسلا", "ولوو", "تویوتا", "فورد", "مجموع"]}
-				colHeaders={["2019", "2020", "2021"]}
+				licenseKey="non-commercial-and-evaluation"
+				rowHeaders={true}
+				colHeaders={true}
 				height={"auto"}
-				colWidths={300}
 				width={1000}
+				rowHeaderWidth={100}
+				rowHeights={40}
 				autoWrapRow={true}
 				autoWrapCol={true}
 				manualColumnResize={true}
@@ -33,32 +53,13 @@ const App = () => {
 				contextMenu={true}
 				manualColumnFreeze={true}
 				dragToScroll={true}
-				columnSummary={[
-					{
-						sourceColumn: 0,
-						type: "sum",
-						reversedRowCoords: true,
-						destinationRow: 0,
-						destinationColumn: 0
-					},
-					{
-						sourceColumn: 1,
-						type: "sum",
-						reversedRowCoords: true,
-						destinationRow: 0,
-						destinationColumn: 1
-					},
-					{
-						sourceColumn: 2,
-						type: "sum",
-						reversedRowCoords: true,
-						destinationRow: 0,
-						destinationColumn: 2
-					}
-				]}
-				rowHeaderWidth={100}
 				enterBeginsEditing={true}
-				columns={[{type: "numeric", readOnly: true}, {type: "numeric"}, {type: "numeric"}]}
+				navigableHeaders={true}
+				columnSorting={true}
+				hiddenColumns={true}
+				data={data}
+				className="htCenter htMiddle"
+				autoColumnSize={true}
 			/>
 		</div>
 	);
